f90a1840761e8f633b6539772576d7cf4547d8c6
Nicholas Cullmann <cullma57@students.rowan.edu>
2021-10-17 22:17:06 -0400

    Regenerated jar

--- src/interaction/InputListener.java
+++ src/interaction/InputListener.java
@@ -35,6 +35,10 @@ public class InputListener implements NativeKeyListener, NativeMouseListener {
             switch (in) {
                 case "Backspace" -> System.out.print("\b");
                 case "Space", "Enter" -> System.out.print(" ");
+//                case "Comma", "Period" -> {
+//                        virtualInput.typeCharacter('\b');
+//                        virtualInput.typeCharacter(in.equals("Comma") ? ',' : '.');
+//                }
             }
         } else {
             try {
